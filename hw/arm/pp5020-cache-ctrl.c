93ae75035bf8656da8901824b6c4c5e040b23e80
Roman Didukh <roma.didukh@gmail.com>
2025-04-11 00:34:55 +0100

    Always report cache as disabled.

--- hw/arm/pp5020-cache-ctrl.c
+++ hw/arm/pp5020-cache-ctrl.c
@@ -5,9 +5,15 @@
 
 static uint64_t pp5020_cache_ctrl_read(void *opaque, hwaddr addr,
                                        unsigned size) {
-  info_report("pp5020_cache_ctrl_read: addr=0x%lx, callee: %d pc=0x%lx", addr,
+  info_report("pp5020_cache_ctrl_read: addr=0x%lx, callee=%d, pc=0x%lx", addr,
               current_cpu->cpu_index, current_cpu->cc->get_pc(current_cpu));
   // TODO: PP5020CacheCtrlState *state = PP5020_CACHE_CTRL(opaque);
+
+  if (addr == 0) {
+    // Always report cache as disabled.
+    // TODO: implement if ever needed.
+    return 0;
+  }
 
   assert(false);
   return 0;
@@ -18,6 +24,13 @@ static void pp5020_cache_ctrl_write(void *opaque, hwaddr addr, uint64_t data,
   info_report(
       "pp5020_cache_ctrl_write: addr=0x%lx, data=0x%lx callee: %d pc=0x%lx",
       addr, data, current_cpu->cpu_index, current_cpu->cc->get_pc(current_cpu));
+
+  if (addr == 0) {
+    // Ignore any cache control commands.
+    // TODO: implement if ever needed.
+    return;
+  }
+
   assert(false);
 }
 
